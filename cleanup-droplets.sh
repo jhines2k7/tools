9a1671141701d06142f0e1b46b25eedf9ed19904
James Hines <jameshines10@gmail.com>
2023-09-26 21:37:25 +0000

    Updates the script for cleaning up droplets

--- cleanup-droplets.sh
+++ cleanup-droplets.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
-# Get the list of all running droplets
-droplets=$(doctl compute droplet list --format "ID,Name,Status" | grep -e "active\b")
+# This script uses doctl to interact with the DigitalOcean API.
+# Make sure it's available in PATH
 
-# Check if there are any droplets to delete
-if [ -z "$droplets" ]; then
-    echo "No droplets to delete."
-    exit 0
-fi
+# setenv DIGITALOCEAN_ACCESS_TOKEN abc123abc123abc123abc123
+# Change above line with your personal access token
 
-# Loop through the droplets
-while IFS= read -r droplet; do
-  droplet_id=$(echo "$droplet" | awk '{print $1}')
-  droplet_name=$(echo "$droplet" | awk '{print $2}')
+DATE=$(date +%Y-%m-%d)
 
-  # Skip droplets with name "jump-server-1" and ID "347233147"
-  if [[ "$droplet_name" == "jump-server-1" && "$droplet_id" == "347233147" ]]; then
-    continue
-  fi
+IFS=$'\n'
+for droplet in $(doctl compute droplet list --format "ID,Name,Status" --no-header); do
+  droplet_id=$(echo $droplet | cut -d ' ' -f1)
+  droplet_name=$(echo $droplet | cut -d ' ' -f2)
+  droplet_status=$(echo $droplet | cut -d ' ' -f3)
 
-  # Stop the droplet
-  echo "Stopping droplet: $droplet_name"
-  doctl compute droplet-action shutdown "$droplet_id"
+  if [ $droplet_name != 'jump-server-1' ] && [ $droplet_id != 347233147 ] && [ $droplet_status == 'active' ]; then
+    echo "Working on Droplet:  id: $droplet_id  name: $droplet_name  status: $droplet_status"
+    echo "==================================================================================="
 
-  # Wait for the droplet to shut down
-  while true; do
-    status=$(doctl compute droplet get "$droplet_id" --format "Status" --no-header --output json | jq -r '.[0].status')
-    if [["$status" == "off" ]]; then
-      break
+    # Snapshot the droplet, wait for the task, and log the produced snapshot ID
+    echo "Creating a snapshot of the droplet..."
+    snapshot=$(doctl compute droplet-action snapshot "$droplet_id" --snapshot-name "$droplet_name-$DATE" --wait)
+
+    if [ $? -eq 0 ]; then
+      snapshot_id=$(echo $snapshot | tail -1 | awk '{print $3}')
+      echo "Snapshot successful. Snapshot ID: $snapshot_id"
+
+      # Delete the droplet and check if deletion was successful
+      echo "Deleting the droplet..."
+      if doctl compute droplet delete "$droplet_id" -f; then
+        echo "Droplet deletion successful. Droplet ID: $droplet_id"
+      else
+        echo "Droplet deletion failed."
+      fi
+    else
+      echo "Snapshot creation failed."
     fi
-    sleep 5
-  done
-
-  # Create a snapshot
-  snapshot_name="snapshot-$(date +"%Y%m%d%H%M%S")-$droplet_name"
-  echo "Creating snapshot: $snapshot_name"
-  doctl compute droplet-action snapshot "$droplet_id" --snapshot-name "$snapshot_name"
-
-  # Delete the droplet
-  echo "Deleting droplet: $droplet_name"
-  doctl compute droplet delete "$droplet_id" --force
-done <<< "$droplets"
+    echo "==================================================================================="
+    echo
+  fi
+done
